99156f72819458251564c7c2efe24169ed4673f5
ElenaGeorgievska <elena_science@yahoo.com>
2023-07-29 16:42:25 +0200

    stage0 module4 conditions task1 completed

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+
+        if(true){
+            System.out.println("True!");
+        }else{
+            System.out.println("False!");
+        }
     }
+
+    //Create a program that will print "true" to console.
+    //Printing command should be placed inside if block which will have "true" in condition part.
+    //Program should be implemented in code snippet:
 }
